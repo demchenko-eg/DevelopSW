264a80d13679217a79dfd2e2282f7c6d1ecea2c8
Yehor Demchenko <dem4enkoegor@knu.ua>
2024-01-27 16:46:29 +0200

    Commit 3

--- main.kt
+++ main.kt
@@ -2,8 +2,14 @@ fun printHelloWorld() {
     println("Hello, World")
 }
 
+fun vivaMechMat() {
+    println("Viva Mech-mat faculty")
+}
+
 fun main() {
     repeat(10) {
         printHelloWorld()
     }
+
+    vivaMechMat()
 }
