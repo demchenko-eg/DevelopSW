30ceb66d02aca4db4fd295feb0926fae57aed578
Yehor Demchenko <dem4enkoegor@knu.ua>
2024-02-17 10:36:52 +0200

    Pow2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from utils import is_prime
+from utils import is_prime, pow5, pow2
 
 n = int(input())
 # print('Факторіал введеного числа:' f(n))
 print(is_prime(n))
+print(pow5(n))
+print(pow2(n))
--- utils.py
+++ utils.py
@@ -7,3 +7,17 @@ def is_prime(number):
         if number % i == 0:
             return False
     return True
+
+def pow5(n):
+    while n >= 5:
+        if n % 5 != 0:
+            return False
+        n //= 5
+    return True
+
+def pow2(n):
+    while n >= 2:
+        if n % 2 != 0:
+            return False
+        n //= 2
+    return True
