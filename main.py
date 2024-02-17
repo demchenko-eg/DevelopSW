f3611b1042b92afb689e579c9bd14409103e8ae8
Yehor Demchenko <dem4enkoegor@knu.ua>
2024-02-17 10:32:24 +0200

    Pow5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils import is_prime
+from utils import is_prime, pow5
 
 n = int(input())
 # print('Факторіал введеного числа:' f(n))
 print(is_prime(n))
+print(pow5(n))
--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 from math import factorial as f
 
-def is_prime(number):
-    if number < 2:
+def is_prime(n):
+    if n < 2:
         return False
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
+    return True
+
+def pow5(n):
+    while n >= 5:
+        if n % 5 != 0:
+            return False
+        n //= 5
     return True
